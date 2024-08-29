1b6cd3257cacfb50ef4e4cab58380a3daa145a58
jiangjingzhe <127188743+jiangjingzhe@users.noreply.github.com>
2024-08-29 22:14:45 +0200

    done with proj1

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -56,10 +56,10 @@ public class ArrayDeque<T> implements Deque<T>, Iterable<T>{
         rear = (rear + 1) % capacity;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
+//    @Override
+//    public boolean isEmpty() {
+//        return size == 0;
+//    }
 
     @Override
     public int size() {
